3d374dc28c2f006d20add6cb96650631efdef999
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2025-10-14 20:38:47 +0300

    Updates

---
+++ assets/Container-BtKvtEGr.js
@@ -0,0 +1,5 @@
+import{s as a}from"./index-DDwkMyRQ.js";const i=a.div`
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0 15px;
+`;export{i as C};
---
+++ assets/MoviesList-CQfWt9fB.js
@@ -0,0 +1,4 @@
+import{s as l,a,j as s,L as r}from"./index-DDwkMyRQ.js";const c=l.ul`
+  list-style: none;
+  text-align: left;
+`,x=({items:e})=>{const o=a(),n=e.map(({id:t,title:i})=>s.jsx("li",{children:s.jsx(r,{to:`/movies/${t}`,state:{from:o},children:i})},t));return s.jsx(c,{children:n})};export{x as M};
